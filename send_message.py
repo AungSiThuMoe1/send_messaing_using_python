495f470a270483ea748ac3dd11a230cddb0794a7
Aung Si Thu Moe <aungsithumoe23@gmail.com>
2024-10-14 21:31:01 +0900

    Add files via upload
    initial commit

---
+++ send_message.py
@@ -0,0 +1,119 @@
+import json
+import argparse
+import google.auth.transport.requests
+import requests
+from google.oauth2 import service_account
+PROJECT_ID = '<YOUR-PROJECT-ID>'
+BASE_URL = 'https://fcm.googleapis.com'
+FCM_ENDPOINT = 'v1/projects/' + PROJECT_ID + '/messages:send'
+FCM_URL = BASE_URL + '/' + FCM_ENDPOINT
+SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
+
+def _get_access_token():
+  """Retrieve a valid access token that can be used to authorize requests.
+
+  :return: Access token.
+  """
+  credentials = service_account.Credentials.from_service_account_file(
+    'service-account.json', scopes=SCOPES)
+  request = google.auth.transport.requests.Request()
+  credentials.refresh(request)
+  return credentials.token
+
+def _send_fcm_message(fcm_message):
+  """Send HTTP request to FCM with given message.
+
+  Args:
+    fcm_message: JSON object that will make up the body of the request.
+  """
+  # [START use_access_token]
+  headers = {
+    'Authorization': 'Bearer ' + _get_access_token(),
+    'Content-Type': 'application/json; UTF-8',
+  }
+  # [END use_access_token]
+  resp = requests.post(FCM_URL, data=json.dumps(fcm_message), headers=headers)
+
+  if resp.status_code == 200:
+    print('Message sent to Firebase for delivery, response:')
+    print(resp.text)
+  else:
+    print('Unable to send message to Firebase')
+    print(resp.text)
+
+def _build_common_message():
+  """Construct common notifiation message.
+
+  Construct a JSON object that will be used to define the
+  common parts of a notification message that will be sent
+  to any app instance subscribed to the news topic.
+  """
+  return {
+    'message': {
+      'token': '<device token>',
+      'notification': {
+        'title': 'FCM Notification',
+        'body': 'Notification from FCM',
+      },
+      'data': {
+        'title': 'FCM Notification',
+        'body': 'Notification from FCM',
+      },
+    }
+  }
+
+def _build_override_message():
+  """Construct common notification message with overrides.
+
+  Constructs a JSON object that will be used to customize
+  the messages that are sent to iOS and Android devices.
+  """
+  fcm_message = _build_common_message()
+
+  apns_override = {
+    'payload': {
+      'aps': {
+        "badge": 0,
+        "sound": "default",
+        "mutable-content": 1,
+        "content-available": 1
+      }
+    },
+    'headers': {
+      'apns-priority': '10'
+    }
+  }
+
+  android_override = {
+    'notification': {
+      'click_action': 'android.intent.action.MAIN',
+      "sound": "default"
+    }
+  }
+
+  fcm_message['message']['android'] = android_override
+  fcm_message['message']['apns'] = apns_override
+
+  return fcm_message
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--message')
+  args = parser.parse_args()
+  if args.message and args.message == 'common-message':
+    common_message = _build_common_message()
+    print('FCM request body for message using common notification object:')
+    print(json.dumps(common_message, indent=2))
+    _send_fcm_message(common_message)
+  elif args.message and args.message == 'override-message':
+    override_message = _build_override_message()
+    print('FCM request body for override message:')
+    print(json.dumps(override_message, indent=2))
+    _send_fcm_message(override_message)
+  else:
+    print('''Invalid command. Please use one of the following commands:
+python messaging.py --message=common-message
+python messaging.py --message=override-message''')
+
+if __name__ == '__main__':
+  main()
